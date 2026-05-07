08d8e6911926f771c911cd976cb7e7e541c1d34c
Yureylis <Yureylis@gmail.com>
2026-05-06 20:05:05 -0400

    cambio

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $cartCount = array_sum(array_column($_SESSION['cart'], 'quantity'));
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>FarmaExpress</title>
   <link rel="shortcut icon" href="assets/img/logo.webp" type="image/x-icon">
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="styls.css" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
